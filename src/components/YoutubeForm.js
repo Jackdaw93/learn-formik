81c3f385cf18f4b7d20e2cf2ce17b9c794541c74
Rohmad <rohmad37@ymail.com>
2020-08-12 18:37:40 +0700

    update, boileparte

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
 import * as Yup from "yup";
+import TextError from "./TextError";
 
 const initialValues = {
   name: "",
   email: "",
   channel: "",
   comments: "",
+  address: "",
+  social: {
+    facebook: "",
+    twitter: "",
+  },
+  phoneNumbers: ["", ""],
+  phNumbers: [""],
 };
 
 const onSubmit = (values) => {
@@ -30,12 +38,14 @@ function YoutubeForm() {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
           <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email">
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
+          </ErrorMessage>
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
@@ -50,6 +60,66 @@ function YoutubeForm() {
         <div className="form-control">
           <label htmlFor="comments">Comments</label>
           <Field as="textarea" id="comments" name="comments" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, meta } = props;
+              console.log("Render Props", props);
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
+          <div className="form-control">
+            <label htmlFor="facebook">Facebook profile</label>
+            <Field type="text" id="facebook" name="social.facebook" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="twitter">Twitter profile</label>
+            <Field type="text" id="twitter" name="social.twitter" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="primaryPh">Primary phone number</label>
+            <Field type="text" id="primaryPh" name="phoneNumbers[0]" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="secondaryPh">Secondary phone number</label>
+            <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
+          </div>
+        </div>
+        <div className="form-control">
+          <label>List od phone numbers</label>
+          <FieldArray name="phNumbers">
+            {(fieldArrayProps) => {
+              console.log("fieldArrayProps", fieldArrayProps);
+              const { push, remove, form } = fieldArrayProps;
+              const { values } = form;
+              const { phNumbers } = values;
+              return (
+                <div>
+                  {phNumbers.map((phNumber, index) => (
+                    <div key={index}>
+                      <Field name={`phNumbers[${index}]`} />
+                      {index > 0 && (
+                        <button type="button" onClick={() => remove(index)}>
+                          -
+                        </button>
+                      )}
+                      <button type="button" onClick={() => push("")}>
+                        +
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+          </FieldArray>
         </div>
         <button type="submit">Submit</button>
       </Form>
